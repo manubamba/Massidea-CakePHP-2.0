5879d2431ac01fab219f301e4e657021535da7fa
Manu Bamba <manu.bamba@gmail.com>
2011-11-09 15:56:42 +0200

    Added missing folders

---
+++ app/Config/database.php
@@ -0,0 +1,20 @@
+<?php
+class DATABASE_CONFIG {
+
+	public $test = array(
+		'datasource' => 'Database/',
+		'persistent' => false,
+		'host' => 'localhost',
+		'login' => 'user',
+		'password' => 'password',
+		'database' => 'test_database_name',
+	);
+	public $default = array(
+		'datasource' => 'Database/Mysql',
+		'persistent' => false,
+		'host' => 'localhost',
+		'login' => 'oibs',
+		'password' => '',
+		'database' => 'massidea2',
+	);
+}
